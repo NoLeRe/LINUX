00cb1fe212f3ae7ec06bdca59e00f379174ffd02
Your Name <1179451475@qq.com>
2020-03-12 19:59:39 +0800

    子进程创建

---
+++ test_3_12/test.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<unistd.h>
+
+
+int main()
+{
+	printf("创建进程\n");
+	pid_t pid; //定义这个变量只是开辟一块空间
+	pid= fork();//这个函数的返回值对于父子进程不一样
+
+	if(pid==0)
+	{
+		printf("这是一个子进程\n");
+	}
+	else
+		printf("这是一个父进程\n");
+	printf("进程创建完毕\n");
+	return 0;
+}
