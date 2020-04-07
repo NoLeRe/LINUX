b8e21dae75822599d267cc0360b97ac922aea73d
Your Name <1179451475@qq.com>
2020-04-07 15:42:47 +0800

    共享内存的简单实现

---
+++ shm/write.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/shm.h>
+#include<unistd.h>
+
+#define IPC_KEY 0x12345678
+
+int main()
+{
+	int shm_id=shmget(IPC_KEY,32,IPC_CREAT|0664);
+	if(shm_id<0)
+	{
+		perror("shmget error");
+		return -1;
+	}
+	void *shm_start=shmat(shm_id,NULL,0);
+	if(shm_start==(void*)-1)
+	{
+		perror("shmat error");
+		return -1;
+	}
+	int i=0;
+	while(1)
+	{
+		sleep(1);
+		sprintf(shm_start,"%s+%d","hello world",i++);		
+	}
+	shmdt(shm_start);
+	shmctl(shm_id,IPC_RMID,NULL);
+
+
+	return 0;	
+}
